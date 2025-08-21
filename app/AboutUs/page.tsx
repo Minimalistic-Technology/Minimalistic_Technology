b04b45b63bf352931635ace723002f185fa8338a
Devansh Raghav <Devanshraghavsn@gmail.com>
2025-08-21 13:54:23 +0530

    removed extra pages

--- app/AboutUs/page.tsx
+++ app/AboutUs/page.tsx
@@ -141,7 +141,7 @@ const AboutUs = () => {
         </section>
 
         {/* Call to Action */}
-        <div className="flex flex-col items-center justify-center text-center mt-10">
+        <div className="flex flex-col items-center justify-center text-center my-10">
           <div className="w-full max-w-full bg-gradient-to-r from-[#23272A] to-[#87C732] py-10 shadow-lg">
             <p className="text-4xl dark:text-white text:black font-semibold">
               Let's build something amazing together
@@ -154,90 +154,7 @@ const AboutUs = () => {
           </div>
         </div>
 
-        {/* Footer */}
-        {/* <footer className="bg-white text-black dark:bg-[#23272A] dark:text-gray-300 mt-10">
-          <div className="max-w-full mx-auto px-6">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-6">
-              <div>
-                <h3 className="dark:text-white text:black font-semibold text-lg mb-3">
-                  Minimalistic Technology
-                </h3>
-                <ul className="space-y-2">
-                  <li className="hover:text-black dark:hover:text-white cursor-pointer">
-                    <Link href="/Team">Our Team</Link>
-                  </li>
-
-                  <li>
-                    <a
-                      href="#"
-                      className="hover:text-black dark:hover:text-white transition"
-                    >
-                      Services
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="hover:text-black dark:hover:text-white transition"
-                    >
-                      Contact Us
-                    </a>
-                  </li>
-                </ul>
-              </div>
-
-              <div>
-                <h3 className="dark:text-white text:black dark:hover:text-white font-semibold text-lg mb-3">
-                  Services
-                </h3>
-                <ul className="space-y-2">
-                  <li>
-                    <a
-                      href="#get-started-section"
-                      className="hover:text-white transition"
-                    >
-                      Book a Service
-                    </a>
-                  </li>
-              
-                </ul>
-              </div>
-            </div>
-
-            
-            <div className="dark:text-white text:black text-sm h-6 mt-6">
-              © 2025 Minimalistic Technology. All Rights Reserved.
-            </div>
-            <div className="flex space-x-4 justify-end mt-4 mb-5">
-              <a
-                href="https://x.com/TechMinimalists"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="dark:text-white text:black dark:hover:text-white transition"
-              >
-                <FaTwitter size={24} />
-              </a>
-
-              <a
-                href="https://www.instagram.com/minimalistictechnology?utm_source=qr&igsh=MTRucHg5bm5wNDBnYg=="
-                target="_blank"
-                rel="noopener noreferrer"
-                className="dark:text-white text:black dark:hover:text-white transition"
-              >
-                <FaInstagram size={24} />
-              </a>
-
-              <a
-                href="https://www.linkedin.com/showcase/minimalistictechnology/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="dark:text-white text:black dark:hover:text-white transition"
-              >
-                <FaLinkedin size={24} />
-              </a>
-            </div>
-          </div>
-        </footer> */}
+        
       </div>
     </div>
     </div>
